747adcf26e0015bf651169bacc556b38f2753d10
RedTachyon <jakub.kwiatkowski.96@gmail.com>
2019-11-29 23:32:47 +0200

    Removed recurrent models support, added checkpointing models every 50 steps

--- agents.py
+++ agents.py
@@ -4,7 +4,7 @@ import torch
 from torch import nn, Tensor
 from torch.distributions import Distribution, Categorical
 
-from models import BaseModel, MLPModel, LSTMModel
+from models import BaseModel, MLPModel
 
 from typing import Tuple, Union, List
 
@@ -12,28 +12,24 @@ from typing import Tuple, Union, List
 class Agent:
     def __init__(self, model: BaseModel, name: str):
         self.model = model
-        self.stateful = model.stateful
         self.name = name
 
         self.storage = {}
 
     def compute_actions(self, obs_batch: Tensor,
-                        state_batch: Tuple = (),
-                        deterministic: bool = False) -> Tuple[Tensor, Tensor, Tuple]:
+                        deterministic: bool = False) -> Tuple[Tensor, Tensor]:
         """
         Computes the action for a batch of observations with given hidden states. Breaks gradients.
 
         Args:
             obs_batch: observation array in shape either (batch_size, obs_size)
-            state_batch: tuple of state tensors of shape (batch_size, lstm_nodes)
             deterministic: boolean, whether to always take the best action
 
         Returns:
             action, logprob of the action, new state vectors
         """
         action_distribution: Categorical
-        states: Tuple
-        action_distribution, _, states = self.model(obs_batch, state_batch)
+        action_distribution, _ = self.model(obs_batch)
         if deterministic:
             actions = torch.argmax(action_distribution.probs, dim=-1)
         else:
@@ -41,11 +37,10 @@ class Agent:
 
         logprobs = action_distribution.log_prob(actions)
 
-        return actions, logprobs, states
+        return actions, logprobs
 
-    def compute_single_action(self, obs: np.ndarray,  # TODO: consider returning the entropy here as well for metrics?
-                              state: Tuple = (),
-                              deterministic: bool = False) -> Tuple[int, float, Tuple]:
+    def compute_single_action(self, obs: np.ndarray,
+                              deterministic: bool = False) -> Tuple[int, float]:
         """
         Computes the action for a single observation with the given hidden state. Breaks gradients.
 
@@ -59,9 +54,9 @@ class Agent:
         """
         obs = torch.tensor([obs])
 
-        action, logprob, new_state = self.compute_actions(obs, state, deterministic)
+        action, logprob = self.compute_actions(obs, deterministic)
 
-        return action.item(), logprob.item(), new_state
+        return action.item(), logprob.item()
 
     def evaluate_actions(self, obs_batch: Tensor,
                          action_batch: Tensor) -> Tuple[Tensor, Tensor, Tensor]:
@@ -79,10 +74,8 @@ class Agent:
             values: tensor of observation values (batch_size, )
             entropies: tensor of entropy values (batch_size, )
         """
-        state = self.get_initial_state()
 
-
-        action_distribution, values, new_states = self.model(obs_batch, state)
+        action_distribution, values = self.model(obs_batch)
         action_logprobs = action_distribution.log_prob(action_batch)
         entropies = action_distribution.entropy()
 
--- models.py
+++ models.py
@@ -12,18 +12,10 @@ from utils import with_default_config, get_activation
 class BaseModel(nn.Module):
     def __init__(self, config: Dict):
         super().__init__()
-        self._stateful = False
         self.config = config
 
-    def forward(self, x: Tensor, state: Tuple) -> Tuple[Distribution, Tensor, Tuple[Tensor, Tensor]]:
+    def forward(self, x: Tensor) -> Tuple[Distribution, Tensor]:
         raise NotImplementedError
-
-    def get_initial_state(self) -> Tuple:
-        raise NotImplementedError
-
-    @property
-    def stateful(self):
-        return self._stateful
 
 
 class MLPModel(BaseModel):
@@ -53,7 +45,7 @@ class MLPModel(BaseModel):
         self.policy_head = nn.Linear(layer_sizes[-1], num_actions)
         self.value_head = nn.Linear(layer_sizes[-1], 1)
 
-    def forward(self, x: Tensor, state: Tuple = ()) -> Tuple[Distribution, Tensor, Tuple[Tensor, Tensor]]:
+    def forward(self, x: Tensor) -> Tuple[Distribution, Tensor]:
         x = x.view((x.shape[0], -1))
         # noinspection PyTypeChecker
         for layer in self.hidden_layers:
@@ -65,10 +57,7 @@ class MLPModel(BaseModel):
 
         action_distribution = Categorical(logits=action_logits)
 
-        return action_distribution, value, state
-
-    def get_initial_state(self):
-        return ()
+        return action_distribution, value
 
 
 class CoordConvModel(BaseModel):
@@ -100,7 +89,7 @@ class CoordConvModel(BaseModel):
         self.policy_head = nn.Linear(4*4*64, self.config["num_actions"])
         self.value_head = nn.Linear(4*4*64, 1)
 
-    def forward(self, x: Tensor, state: Tuple = ()):
+    def forward(self, x: Tensor):
         batch_size = x.shape[0]
         batch_coords = torch.stack([self.coords for _ in range(batch_size)], dim=0)
         batch_coords = batch_coords.to(x.device.type)
@@ -118,88 +107,11 @@ class CoordConvModel(BaseModel):
 
         action_distribution = Categorical(logits=action_logits)
 
-        return action_distribution, value, state
-
-    def get_initial_state(self):
-        return ()
-
-
-class LSTMModel(BaseModel):
-    def __init__(self, config: Dict):
-        super().__init__(config)
-
-        self._stateful = True
-
-        default_config = {
-            "input_size": 15,
-            "num_actions": 5,
-            "pre_lstm_sizes": (32,),
-            "lstm_nodes": 32,
-            "post_lstm_sizes": (32,),
-            "activation": "leaky_relu"
-        }
-        self.config = with_default_config(config, default_config)
-
-        # Unpack the config
-        input_size: int = self.config.get("input_size")
-        num_actions: int = self.config.get("num_actions")
-        pre_lstm_sizes: Tuple[int] = self.config.get("pre_lstm_sizes")
-        lstm_nodes: int = self.config.get("lstm_nodes")
-        post_lstm_sizes: Tuple[int] = self.config.get("post_lstm_sizes")
-        self.activation: Callable = get_activation(self.config.get("activation"))
-
-        pre_layers = (input_size,) + pre_lstm_sizes
-        post_layers = (lstm_nodes,) + post_lstm_sizes
-
-        self.preprocess_layers = nn.ModuleList([
-            nn.Linear(in_size, out_size)
-            for in_size, out_size in zip(pre_layers, pre_layers[1:])
-        ])
-
-        self.lstm = nn.LSTMCell(input_size=pre_layers[-1],
-                                hidden_size=lstm_nodes,
-                                bias=True)
-
-        self.postprocess_layers = nn.ModuleList([
-            nn.Linear(in_size, out_size)
-            for in_size, out_size in zip(post_layers, post_layers[1:])
-        ])
-
-        self.policy_head = nn.Linear(post_layers[-1], num_actions)
-        self.value_head = nn.Linear(post_layers[-1], 1)
-
-    def forward(self, x: Tensor, state: Tuple[Tensor, Tensor]) -> Tuple[Distribution, Tensor, Tuple[Tensor, Tensor]]:
-        # noinspection PyTypeChecker
-        for layer in self.preprocess_layers:
-            x = layer(x)
-            x = self.activation(x)
-
-        if len(state) == 0:
-            state = self.get_initial_state()
-
-        (h_state, c_state) = self.lstm(x, state)
-
-        x = h_state
-
-        # noinspection PyTypeChecker
-        for layer in self.postprocess_layers:
-            x = layer(x)
-            x = self.activation(x)
-
-        action_logits = self.policy_head(x)
-        value = self.value_head(x)
-
-        action_distribution = Categorical(logits=action_logits)
-
-        return action_distribution, value, (h_state, c_state)
-
-    def get_initial_state(self) -> Tuple[Tensor, Tensor]:
-        return torch.zeros(1, self.config['lstm_nodes'], requires_grad=True), \
-               torch.zeros(1, self.config['lstm_nodes'], requires_grad=True)
+        return action_distribution, value
 
 
 if __name__ == '__main__':
-    policy = LSTMModel({})
+    policy = MLPModel({})
     data = torch.randn(2, 15)
 
-    action_dist, value_, state_ = policy(data, ())
+    action_dist, value_ = policy(data, ())
